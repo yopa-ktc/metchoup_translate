ff95cf5836c9bf555ef62432d577eb128e0f76d8
yopa-ktc <yoparemy@gmail.com>
2023-05-18 19:04:13 +0200

    la concatenation en SQL

--- nlp/main.py
+++ nlp/main.py
@@ -1,11 +1,40 @@
-import os
+import csv
+import io
+import json
+import mysql.connector
 
-# repertoire_courant = os.getcwd()
-# print(repertoire_courant)
+# Ouvrir les fichiers source en mode lecture
+test_fr = open("test.fr", "r", encoding="utf-8")
+test_gho = open("test.bbj", "r", encoding="utf-8")
 
-fichier = open("./nlp/test.fr", "r", encoding="utf-8")
-contenu = fichier.read()
+# Lire le contenu des fichiers source
+contenu_fr = test_fr.readlines()
+contenu_gho = test_gho.readlines()
 
-print(contenu)
+
+# Fermer les fichiers sources
+test_fr.close()
+test_gho.close()
+
+#Stockage dans une base de données SQL
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="gho_fr"
+)
+cursor = mydb.cursor()
 
 
+#Stockage du ghomala
+for textgho, textfr in zip(contenu_gho, contenu_fr):
+    sql = "INSERT INTO test (gho, fr) VALUES (%s, %s)"
+    val = (textgho.strip(), textfr.strip())
+    cursor.execute(sql, val)
+    
+cursor.close()
+mydb.commit()
+mydb.close()
+#Fermeture de la connexion SQL
+print(cursor.rowcount, "enregistrement inséré.")
